2c50117c649eb3205d26bcd2370cfaddae86f5af
Adam DAIA <98329603+AdaamD@users.noreply.github.com>
2023-12-30 17:17:19 +0100

    VM ok

--- Projet/Lisp PR2/lisp-compiler/machine.lisp
+++ Projet/Lisp PR2/lisp-compiler/machine.lisp
@@ -1,5 +1,3 @@
-(require "VM/fonctions.lisp")
-
 ; création et initialisation de la machine virtuelle
 ; nom = nom de la machine virtuelle
 (defun vm-creation (&optional (nom 'VM) (taille 150000))
@@ -8,102 +6,98 @@
 	(setf (get nom 'R0) 0) ; init Registre 0        (R0)
     (setf (get nom 'R1) 0) ; init Registre 1        (R1)
     (setf (get nom 'R2) 0) ; init Registre 2        (R2)
-    (setf (get nom 'BP) 10) ; Base Pointer          (BP) -> adresse début pile initialisé à 100 // ce registre est initialiser une fois et on y touche plus
+    (setf (get nom 'BP) 10) ; Base Pointer          (BP) -> adresse début pile initialisé à 100 // ce registre est initialisé une fois et on y touche plus
     (setf (get nom 'SP) 10) ; Stack Pointer         (SP) -> adresse sommet de la pile initialisé à 100
     (setf (get nom 'PC) 100001) ; Program Counter    (PC) -> Registre du conteur ordinal initialisé à 0
-    (setf (get nom 'FP) 10) ; Frame Pointer         (FP) -> définission des blocs de pile pour la structurer et facilité les accès.
+    (setf (get nom 'FP) 10) ; Frame Pointer         (FP) -> définition des blocs de pile pour la structurer et faciliter les accès.
     (setf (get nom 'FLT) 0) ; Flag Lower Than       (FLT)
     (setf (get nom 'FEQ) 0) ; Flag Equal            (FEQ)
-    (setf (get nom 'FGT) 0) ; Flag Greather Than    (FGT)
-	(setf (get nom 'symboleR) (make-hash-table)) ; table symboles resolus
-	(setf (get nom 'referenceNR) (make-hash-table)) ; table references non resolus
+    (setf (get nom 'FGT) 0) ; Flag Greater Than    (FGT)
+	(setf (get nom 'symboleR) (make-hash-table)) ; table symboles résolus
+	(setf (get nom 'referenceNR) (make-hash-table)) ; table références non résolues
 	(setf (get nom 'exitVM) 0)
 	(setf (get nom 'maxStack) 100000)
-	  (format t "Creation réussi de la VM : ~A, de taille: ~A~%" nom taille )
-
+	(format t "Création réussie de la VM : ~A, de taille : ~A~%" nom taille)
 )
 
-; execution de l'instruction courante
-(defun vm-eval-inst (nom instr)
+; exécution de l'instruction courante
+(defun vm-exec-inst (nom instr)
 	(let ((src (cadr instr))
 		  (dest (caddr instr)))
 		(case (car instr)
-			('MOVE	 (machine-move nom src dest))
-			('LOAD	 (machine-load nom src dest))
-			('STORE	 (machine-store nom src dest))
+			('MOVE	 (vm-exec-move nom src dest))
+			('LOAD	 (vm-exec-load nom src dest))
+			('STORE	 (vm-exec-store nom src dest))
 
-			('ADD	 (machine-add nom src dest))
-			('MUL	 (machine-mul nom src dest))
-			('SUB	 (machine-sub nom src dest))
-			('DIV	 (machine-div nom src dest))
+			('ADD	 (vm-exec-add nom src dest))
+			('MUL	 (vm-exec-mul nom src dest))
+			('SUB	 (vm-exec-sub nom src dest))
+			('DIV	 (vm-exec-div nom src dest))
 
-			('INCR	 (machine-incr nom src))
-			('DECR	 (machine-decr nom src))
+			('INCR	 (vm-exec-incr nom src))
+			('DECR	 (vm-exec-decr nom src))
 
-			('PUSH	 (machine-push nom src))
-			('POP	 (machine-pop nom src))
+			('PUSH	 (vm-exec-push nom src))
+			('POP	 (vm-exec-pop nom src))
 
-			('CMP	 (machine-cmp nom src dest))
-			('JPG	 (machine-jgt nom src))
-			('JEQ	 (machine-jeq nom src))
-			('JPP	 (machine-jlt nom src))
-			('JGE	 (machine-jge nom src))
-			('JPE	 (machine-jle nom src))
+			('CMP	 (vm-exec-cmp nom src dest))
+			('JPG	 (vm-exec-jgt nom src))
+			('JEQ	 (vm-exec-jeq nom src))
+			('JPP	 (vm-exec-jlt nom src))
+			('JGE	 (vm-exec-jge nom src))
+			('JPE	 (vm-exec-jle nom src))
 
-			('JMP	 (machine-jmp nom src))
-			('JSR	 (machine-jsr nom src))
-			('RTN	 (machine-rtn nom))
-			('NOP	 (machine-nop nom))
-			('HALT	 (machine-halt nom))
+			('JMP	 (vm-exec-jmp nom src))
+			('JSR	 (vm-exec-jsr nom src))
+			('RTN	 (vm-exec-rtn nom))
+			('NOP	 (vm-exec-nop nom))
+			('HALT	 (vm-exec-halt nom))
 
-			('CONS	 (machine-cons nom src dest))
-			('CAR	 (machine-car nom src ))
-			('CDR	 (machine-cdr nom src ))
+			('CONS	 (vm-exec-cons nom src dest))
+			('CAR	 (vm-exec-car nom src ))
+			('CDR	 (vm-exec-cdr nom src ))
 		)
 	)
 )
 
-;; Exécute le code chargé en mémoire ( exécuté grace a eval-instruction)
+; exécute le code chargé en mémoire (exécuté grâce à vm-exec-inst)
 (defun vm-run-code (nom)
 	(loop while (= (get nom 'exitVM) 0) do
 		(let* ((pc (get-registre nom 'PC)) (instr (get-memoire nom pc)))
-			(progn (vm-eval-inst nom instr)
+			(progn (vm-exec-inst nom instr)
 				(if (= (get-registre nom 'PC) pc)
-					(set-registre nom 'PC (+ pc 1)) ; on incrémente de 1 le registre PC à chaque instruction lu
+					(set-registre nom 'PC (+ pc 1)) ; on incrémente de 1 le registre PC à chaque instruction lue
 					nil
 				)
 			)
 		)	
 	)
 	(printemvm "---- Résultat = " (get-registre nom 'R0))
-	
 )
 
-;; ouvre le fichier de code et le mets e mémoire grace a machine-chargeur
-(defun vm-load-code (nom nomfichier &optional (co 100001))
+; ouvre le fichier de code et le met en mémoire grâce à vm-load-code
+(defun vm-load (nom nomfichier &optional (co 100001))
   (with-open-file (fichier nomfichier :direction :input)
     (let ((instructions (read fichier)))
-      (vm-chargeur nom instructions co)))
+      (vm-load-code nom instructions co)))
   "Succès de chargement"
 )
 
-
-
-(defun vm-chargeur (nom fichier &optional (co 100001))
-	(loop while (not (null fichier)) do
-		(let ((instr (car fichier)))
+(defun vm-load-code (nom instructions &optional (co 100001))
+	(loop while (not (null instructions)) do
+		(let ((instr (car instructions)))
 			(if (null instr)
 				nil
 				(if (eql 'LABEL (car instr))
-					(machine-charger-symb nom (cadr instr) co)
+					(vm-exec-charger-symb nom (cadr instr) co)
 					(progn
-						(set-memoire nom co (machine-resoudre-symb nom instr co))
+						(set-memoire nom co (vm-exec-resoudre-symb nom instr co))
 						(setf co (+ co 1))
 					)
 				)
 			)
 		)
-		(setf fichier (cdr fichier))
+		(setf instructions (cdr instructions))
 	)
 )
 
